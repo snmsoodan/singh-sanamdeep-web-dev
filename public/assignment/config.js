77d8085deb8dc63f1a1aa65ccf0ad1f9525027f2
Sanamdeep Singh <snm91@ccs.neu.edu>
2016-07-12 05:23:54 -0400

    added login controller, created llogin function added error message bound login page model with controller view model home

---
+++ public/assignment/config.js
@@ -0,0 +1,70 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/",{
+                templateUrl:"views/home.html"
+            })
+            .when("/login",{
+                templateUrl:"views/user/login.view.client.html",
+                controller:"LoginController",
+                controllerAs: "model"
+            })
+            .when("/register",{
+                templateUrl:"views/user/register.view.client.html"
+            })
+            .when("/profile",{
+                templateUrl:"views/user/profile.view.client.html"
+            })
+
+
+
+            .when("/website-edit",{
+                templateUrl:"views/website/website-edit.view.client.html"
+            })
+            .when("/website-list",{
+                templateUrl:"views/website/website-list.view.client.html"
+            })
+            .when("/website-new",{
+                templateUrl:"views/website/website-new.view.client.html"
+            })
+
+
+
+            .when("/widget-chooser",{
+                templateUrl:"views/widget/widget-chooser.view.client.html"
+            })
+            .when("/widget-heading",{
+                templateUrl:"views/widget/widget-heading.view.client.html"
+            })
+            .when("/widget-image",{
+                templateUrl:"views/widget/widget-image.view.client.html"
+            })
+            .when("/widget-list",{
+            templateUrl:"views/widget/widget-list.view.client.html"
+            })
+            .when("/widget-youtube",{
+                templateUrl:"views/widget/widget-youtube.view.client.html"
+            })
+
+
+
+            .when("/page-edit",{
+                templateUrl:"views/page/page-edit.view.client.html"
+            })
+            .when("/page-list",{
+                templateUrl:"views/page/page-list.view.client.html"
+            })
+            .when("/page-new",{
+                templateUrl:"views/page/page-new.view.client.html"
+            })
+            .otherwise({
+                redirectTo:"/login"
+            })
+
+    }
+})();
