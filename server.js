b97f37467eef4c43ad778b6d8c62f8d77430b235
Sanamdeep Singh <snm91@ccs.neu.edu>
2016-08-07 17:30:26 -0400

    b

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ var passport = require('passport');
 
 var app = express();
 
+var connectionString = 'mongodb://127.0.0.1:27017/webdev';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var mongoose = require("mongoose");
+mongoose.connect(connectionString);
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
