540b157c0c53db18c07426d8396753f672ac4885
Sanamdeep Singh <snm91@ccs.neu.edu>
2016-08-07 23:42:34 -0400

    publishing to OpenShift

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,7 +1,7 @@
 module.exports=function () {
 
-    var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/cs5610summer1');
+    // var mongoose = require('mongoose');
+    // mongoose.connect('mongodb://localhost/cs5610summer1');
 
     var models={
         userModel:require("./user/user.model.server")(),
