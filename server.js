0eea2332c98febf1d6dbd571df3deed70919bb9d
Sanamdeep Singh <snm91@ccs.neu.edu>
2016-08-12 13:51:52 -0400

    try

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.use(session({ secret: "abcdef" }));
+app.use(session({ secret: process.env.SESSION_SECRET }));
+//app.use(session({ secret: "abcdef" }));
 
 app.use(passport.initialize());
 app.use(passport.session());
